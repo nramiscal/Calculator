5b5b8c7fa4ecf481c3ac8a15f8f9b9d87329bab4
nramiscal <nramiscal@gmail.com>
2017-09-04 21:17:32 -0400

    first commit

---
+++ Calculator.java
@@ -0,0 +1,47 @@
+class Calculator implements java.io.Serializable {
+    private double operandOne;
+    private double operandTwo;
+    private String operation;
+    private double result;
+
+    public Calculator(){
+    }
+
+    // public Calculator(double operandOne, String operation, double operandTwo){
+    //     setOperandOne(operandOne);
+    //     setOperation(operation);
+    //     setOperandTwo(operandTwo);
+    //     performOperation(operandOne, operation, operandTwo);
+    // }
+
+    // getters
+    public double getOperandOne(){ return operandOne; }
+    public double getOperandTwo(){ return operandTwo; }
+    public String getOperation(){ return operation; }
+    public double getResult(){ return result; }
+
+    // setters
+    public void setOperandOne(double opOne){
+        this.operandOne = opOne;
+    }
+    public void setOperandTwo(double opTwo){
+        this.operandTwo = opTwo;
+    }
+    public void setOperation(String o){
+        this.operation = o;
+    }
+    public void setResult(double res){
+        this.result = res;
+    }
+
+    public void performOperation(){
+        if (getOperation() == "+"){
+            setResult(getOperandOne() + getOperandTwo());
+        }
+        else { System.out.println ("Invalid operation."); }
+    }
+
+
+
+
+}
---
+++ CalculatorTest.java
@@ -0,0 +1,12 @@
+class CalculatorTest {
+    public static void main(String[] args){
+
+        Calculator calc = new Calculator();
+
+        calc.setOperandOne(10.5);
+        calc.setOperation("+");
+        calc.setOperandTwo(5.2);
+        calc.performOperation();
+        System.out.println(calc.getResult());
+    }
+}
